503488fe94a2760ecc307f2faf012d1d7bc0e41f
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-30 08:55:22 +0100

    Add class for transactions
    Add a class that will contain transactions (or in this case the votes)
    from users.

---
+++ objects/transaction.py
@@ -0,0 +1,7 @@
+
+class transaction():
+    def __init__(self, username, cpr, politicalParty, fee):
+        self.username = username
+        self.cpr = cpr
+        self.fee = fee
+        self.politicalParty = politicalParty
