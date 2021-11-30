f385848aecb9088450ef11c3fecb9cfd18c59e1c
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-30 13:17:06 +0100

    Add flask app for serving the generated DB
    This flask app will allow users to retrieve a users public key based on
    their cpr number.

---
+++ flaskApp/mainFlask.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from flask import Flask, render_template, Response
+import json
+import sqlite3
+
+
+app = Flask(__name__)
+app.static_folder = "./static"
+app.template_folder = "./templates"
+
+@app.route("/publicKey/<string:cpr>")
+def returnPublicKey(cpr):
+    conn = sqlite3.connect("users.db")
+    cur = conn.cursor()
+    cur.execute("select publicKey from users where cpr=?", (cpr,))
+
+    cprNumber = cur.fetchone()[0]
+
+    conn.close()
+
+    return Response(json.dumps({"cpr" : cprNumber}), mimetype='application/json')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
