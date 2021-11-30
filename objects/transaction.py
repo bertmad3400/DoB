7a914ec7362455df0e129037dc2f43ed9f3a4967
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-30 10:08:28 +0100

    Include signature when creating instance of transaction class
    Any instance of the transaction class will now include a signature, but
    this signature will default to None if not specified. This is done so
    that it's both possible to use the transaction class for creating new
    transactions, but also for verifying old ones in the future, once that
    function has been implemented.

--- objects/transaction.py
+++ objects/transaction.py
@@ -1,7 +1,8 @@
 
 class transaction():
-    def __init__(self, username, cpr, politicalParty, fee):
+    def __init__(self, username, cpr, politicalParty, fee, signature=None):
         self.username = username
         self.cpr = cpr
         self.fee = fee
         self.politicalParty = politicalParty
+        self.signature = signature
