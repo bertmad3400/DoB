5fcb4d9cb9435a8ec1201eed44790ebfdb159f4f
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-30 16:54:13 +0100

    Create class for creating wallet based on existing credentials
    The class "wallet" in the "wallet.py" file will enable users to create a
    wallet instance based on existing credentials.

--- objects/wallet.py
+++ objects/wallet.py
@@ -1,7 +1,17 @@
 from Crypto.PublicKey import RSA
+import requests
+import json
+from base64 import b64decode
+from objects.transaction import transaction
 
 class newWallet():
     def __init__(self):
         privateKey = RSA.generate(2048)
         self.privateKey = privateKey.export_key('PEM')
         self.publicKey = privateKey.public_key().export_key('PEM')
+
+class wallet():
+    def __init__(self, cpr, password):
+        self.cpr = cpr
+        self.publicKey = b64decode(requests.get(f"http://localhost:5000/api/publicKey/{cpr}").json()["publicKey"])
+        self.privateKey = b64decode(requests.get(f"http://localhost:5000/api/privateKey/{cpr}/{password}").json()["privateKey"])
