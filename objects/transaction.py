7c8ae23fbec2fe75537894583b7afe05da978948
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-30 10:47:07 +0100

    Add function for calulating hash of transaction
    Add a function to the transaction class, that allows for calculating the
    hash of the transaction, for use when function for signing message is
    implemented in the future.

--- objects/transaction.py
+++ objects/transaction.py
@@ -1,3 +1,5 @@
+from Crypto.Hash import SHA256
+
 
 class transaction():
     def __init__(self, username, cpr, politicalParty, signature=None):
@@ -5,3 +7,7 @@ class transaction():
         self.cpr = cpr
         self.politicalParty = politicalParty
         self.signature = signature
+
+    def calculateTransactionHash(self):
+        transaction = self.username + self.cpr + self.politicalParty
+        return SHA256.new(transaction.encode())
