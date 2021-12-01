70771f97d49bd003c02328fe979e7eca58d7f9f1
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-01 08:56:03 +0100

    Add function for converting blocks to strings.

--- objects/blocks.py
+++ objects/blocks.py
@@ -1,5 +1,11 @@
+import json
 
 class block():
     def __init__(self, lastBlockHash, transactionList):
         self.lastBlockHash = lastBlockHash
         self.transactionList = transactionList
+        self.proofOfWork = 0
+
+    def toString(self):
+        transactionString = [transaction.toString() for transaction in self.transactionList]
+        return json.dumps({ "lastBlock" : self.lastBlockHash, "transactions" : transactionString, "PoW" : self.proofOfWork })
