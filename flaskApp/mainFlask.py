cde74c6c3c2993daad90d4871c0916ba9a3d21e8
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-02 17:55:15 +0100

    Add function for handling incomming transaction

--- flaskApp/mainFlask.py
+++ flaskApp/mainFlask.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, abort
 import json
 import sqlite3
 
@@ -15,6 +15,25 @@ from objects.chain import chain
 app = Flask(__name__)
 app.static_folder = "./static"
 app.template_folder = "./templates"
+
+def handleNewTransaction(transaction):
+    blockResponse = currentBlock.appendTransaction(transaction)
+
+    verifyFeedback = currentBlock.verifyTransactions()
+    while verifyFeedback != -1:
+        currentBlock.pop(verifyFeedback)
+        verifyFeedback = currentBlock.verifyTransactions()
+
+    if blockResponse == 0:
+        currentBlock.calculatePoW()
+        if not currentChain.append(currentBlock):
+            abort(500)
+        currentBlock = block(currentChain.blockchain[-1].getBlockHash(), [])
+    elif blockResponse == 2:
+        abort(422)
+    elif blockResponse == 3:
+        abort(500)
+
 
 def requestSingleDBEntry(fieldName, identifierFields, identifierValues):
     conn = sqlite3.connect("users.db")
